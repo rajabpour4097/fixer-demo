0d5fc542a4b0e79f9f932896d2b5dd04c40ae146
Mohammad Rajabpour <rajabpour4097@gmail.com>
2020-11-06 23:35:43 +0330

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(name='fixer-demo',
+      version='0.1',
+      description='Fixer service demo package',
+      url='#',
+      author='Mohammad Rajabpour',
+      author_email='rajabpour4097@gmail.com',
+      license='MIT',
+      packages=['fixer'],
+      zip_safe=False)
