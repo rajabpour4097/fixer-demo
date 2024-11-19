dea55a73ec2d2f0a49d103106376d2d3af215ed3
Mohammad Rajabpour <50981979+rajabpour4097@users.noreply.github.com>
2024-11-19 19:22:52 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ from setuptools import setup
 setup(name='fixer-demo',
       version='0.1',
       description='Fixer service demo package',
-      url='#',
+      url='https://github.com/rajabpour4097/fixer-demo',
       author='Mohammad Rajabpour',
       author_email='rajabpour4097@gmail.com',
       license='MIT',
       packages=['fixer'],
+      install_requires=['requests'],
       zip_safe=False)
